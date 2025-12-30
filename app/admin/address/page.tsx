97567b90a529976b22fa50a446efc56e576a6ecf
Amirize Wisdom <97132123+chukwumela909@users.noreply.github.com>
2025-12-30 20:45:24 +0100

    Add default address creation in admin and mobile deposit pages

--- app/admin/address/page.tsx
+++ app/admin/address/page.tsx
@@ -3,12 +3,29 @@ import AddressEdit from "@/components/admin/UserEdits/AddresssEdit";
 import dbConnect from "@/server";
 import Address, { PaymentAddress } from "@/server/addressSchema";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   await dbConnect();
 
-  const rawData = await Address.findOne({ name: "capitalnexusonline" });
+  let rawData = await Address.findOne({ name: "capitalnexusonline" });
+  
+  // Create default document if it doesn't exist
+  if (!rawData) {
+    rawData = await Address.create({
+      name: "capitalnexusonline",
+      bitcoin: "",
+      ethereum: "",
+      litecoin: "",
+      dogecoin: "",
+      cashapp: "",
+      zelle: "",
+      venmo: "",
+      paypal: "",
+    });
+  }
+  
   const data: PaymentAddress = JSON.parse(JSON.stringify(rawData));
-  // console.log(data);
 
   return (
     <div>
